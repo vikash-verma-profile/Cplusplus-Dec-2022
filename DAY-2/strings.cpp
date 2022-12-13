ba521198f76f2b7de314d98e07157610d2cc3f25
vikash-verma-profile <er.vikashverma@yahoo.com>
2022-12-13 15:47:11 +0530

    aded strings

--- DAY-2/strings.cpp
+++ DAY-2/strings.cpp
@@ -4,17 +4,52 @@ using namespace std;
 
 int main(){
 
-    string str;
-    cout<<"Please input some string";
-    getline(cin,str);
+    string str="Vikash Verma is a good boy";
+    // 1 Byte
+    //9 +1 =>10 bytes
+    // cout<<"Please input some string ";
+    // getline(cin,str);
 
     //getline(is,str,delim)
 
-    cout<<"The initial string is :";
+    // cout<<"The initial string is :";
+    // cout<<str<<endl;
+    // str.push_back('v');
+    // cout<<"After apending the character"<<str;
+    // str.pop_back();
+    // cout<<"After deleting the character"<<str;
+
+    // str.resize(18);
+    // cout<<"After resizing ";
+    // cout<<str<<endl;
+    // cout<<str.capacity()<<endl;// it will be return the size of the storage space currenyly allocated
+    // // cout<<str.size();
+    // str.shrink_to_fit();
+    // cout<<str<<endl;
+    // cout<<str.capacity()<<endl;
+
+    //declaring the iterator
+    std::string::iterator it;
+
+    //declare the reverse_iterator
+    std::string::reverse_iterator it1;
+
     cout<<str<<endl;
-    str.push_back('v');
-    cout<<"After apending the character"<<str;
-    str.pop_back();
-    cout<<"After deleting the character"<<str;
+    cout<<"using forward iterator";
+    for(it=str.begin();it!=str.end();it++){
+       if(it==str.begin())*it='v';
+        cout<<*it;
+    }
+    cout<<endl;
     return 0;
 }
+
+
+//string functions
+/*
+-begin()
+-end()
+-rbegin()
+-rend()
+
+*/
